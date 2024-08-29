9b15c2c4d9f4d9bee9b3c29f2c8d74b24ba69785
josevegara04 <jbvegar@eafit.edu.co>
2024-08-28 19:26:57 -0500

    add grafica.html and change views.py and homepage.html

--- Calculadora/Calculator/views.py
+++ Calculadora/Calculator/views.py
@@ -6,6 +6,7 @@ import math
 import io
 import matplotlib.pyplot as plt
 import numpy as np
+import base64
 
 def Calculator(request):
     nombre = metodo.objects.all()
@@ -15,11 +16,35 @@ def Calculator(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def funcion(request):
-    fig, ax = plt.subplots()
-    x = np.arange(10.0)
-    ax.plot(x)
+    template = loader.get_template("grafica.html")
+    ecuacion = request.GET.get('function', '2*x + 1') 
+
+    x = np.linspace(-10, 10, 400)
+    
+    try:
+        y = eval(ecuacion)
+    except Exception as e:
+        return HttpResponse(f"Error al evaluar la función: {e}")
+
+    plt.figure(figsize=(6, 4))
+    plt.plot(x, y, label=f"y = {ecuacion}")
+    
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title(f"Gráfica de la función y = {ecuacion}")
+    plt.legend()
+    plt.grid(True)
+    
     buffer = io.BytesIO()
     plt.savefig(buffer, format="png")
     buffer.seek(0)
-    return HttpResponse(buffer, content_type="image/png")
+    
+    plt.close()
+    
+    image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+    context = {'buffer': image_base64}
+
+    return HttpResponse(template.render(context, request))
