ded81385866dc3438e633f82dbf3b1bce8519dfa
josevegara04 <jbvegar@eafit.edu.co>
2024-08-29 22:58:24 -0500

    add bisection method

--- Calculadora/Calculator/urls.py
+++ Calculadora/Calculator/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Calculator, name='Calculator'),  # La ruta raíz para la app Calculator
-    path('funcion/', views.funcion, name="funcion")
+    path('', views.Calculator, name='Calculator'),
+    path("fun_bisection/", views.fun_bisection, name="fun_bisection")
+    
 ]
